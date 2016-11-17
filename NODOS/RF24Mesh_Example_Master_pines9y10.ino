6ab11757d3644da172869ba4aae2f8124c4f6ed6
cabezadediablo <nicvil@gmail.com>
2016-11-17 15:21:53 +0100

    Update RF24Mesh_Example_Master_pines9y10.ino

--- NODOS/RF24Mesh_Example_Master_pines9y10.ino
+++ NODOS/RF24Mesh_Example_Master_pines9y10.ino
@@ -1,18 +1,16 @@
- 
- 
- /** RF24Mesh_Example_Master.ino by TMRh20
-  * 
-  *
-  * This example sketch shows how to manually configure a node via RF24Mesh as a master node, which
-  * will receive all data from sensor nodes.
-  *
-  * The nodes can change physical or logical position in the network, and reconnect through different
-  * routing nodes as required. The master node manages the address assignments for the individual nodes
-  * in a manner similar to DHCP.
-  *
-  */
-  
-  
+/** RF24Mesh_Example_Master.ino by TMRh20
+
+
+    This example sketch shows how to manually configure a node via RF24Mesh as a master node, which
+    will receive all data from sensor nodes.
+
+    The nodes can change physical or logical position in the network, and reconnect through different
+    routing nodes as required. The master node manages the address assignments for the individual nodes
+    in a manner similar to DHCP.
+
+*/
+
+
 #include "RF24Network.h"
 #include "RF24.h"
 #include "RF24Mesh.h"
@@ -21,11 +19,12 @@
 #include <EEPROM.h>
 
 /***** Configure the chosen CE,CS pins *****/
-RF24 radio(7,8);///pines del CE y CSN creo...
+RF24 radio(7, 8); ///pines del CE y CSN creo...
 RF24Network network(radio);
-RF24Mesh mesh(radio,network);
+RF24Mesh mesh(radio, network);
 
 uint32_t displayTimer = 0;
+int dat[2];
 
 void setup() {
   Serial.begin(115200);
@@ -39,53 +38,86 @@ void setup() {
 }
 
 
-void loop() {    
+void loop() {
 
   // Call mesh.update to keep the network updated
   mesh.update();
-  
+
   // In addition, keep the 'DHCP service' running on the master node so addresses will
   // be assigned to the sensor nodes
   mesh.DHCP();
-  
-  
+
+
   // Check for incoming data from the sensors
-  if(network.available()){
+  if (network.available()) {
     RF24NetworkHeader header;
     network.peek(header);
-    
-    int32_t dat=0;
-    switch(header.type){
+
+    //int32_t dat=0;
+    switch (header.type) {
       // Display the incoming millis() values from the sensor nodes
-      case 'B': network.read(header,&dat,sizeof(dat)); Serial.print("b"); Serial.print(dat); Serial.println("\t");break;
-      case 'C': network.read(header,&dat,sizeof(dat)); Serial.print("c"); Serial.print(dat); Serial.println("\t");break;
-      case 'D': network.read(header,&dat,sizeof(dat)); Serial.print("d"); Serial.print(dat); Serial.println("\t"); break;
-      case 'E': network.read(header,&dat,sizeof(dat)); Serial.print("e"); Serial.print(dat); Serial.println("\t"); break;
-      case 'F': network.read(header,&dat,sizeof(dat)); Serial.print("f"); Serial.print(dat); Serial.println("\t"); break;
-      case 'G': network.read(header,&dat,sizeof(dat)); Serial.print("g"); Serial.print(dat); Serial.println("\t"); break;  
-      case 'H': network.read(header,&dat,sizeof(dat)); Serial.print("h"); Serial.print(dat); Serial.println("\t"); break;
-      case 'I': network.read(header,&dat,sizeof(dat)); Serial.print("i"); Serial.print(dat); Serial.println("\t"); break;
-      case 'j': network.read(header,&dat,sizeof(dat)); Serial.print("j"); Serial.println(dat); break;
-      default: network.read(header,0,0); Serial.println(header.type);break;
+
+      case '1': network.read(header, &dat, sizeof(dat));
+        Serial.print("a");
+        Serial.println(dat[0]);
+        Serial.print("b");
+        Serial.println(dat[1]);
+        Serial.print("c");
+        Serial.println(dat[2]);
+        Serial.println();
+        break;
+      case '2': network.read(header, &dat, sizeof(dat));
+        Serial.print("d");
+        Serial.println(dat[0]);
+        Serial.print("e");
+        Serial.println(dat[1]);
+        Serial.print("f");
+        Serial.println(dat[2]);
+        Serial.println();
+        break;
+      case '3': network.read(header, &dat, sizeof(dat));
+        Serial.print("g");
+        Serial.println(dat[0]);
+        Serial.print("h");
+        Serial.println(dat[1]);
+        Serial.print("i");
+        Serial.println(dat[2]);
+        Serial.println();
+        break;
+      case '4': network.read(header, &dat, sizeof(dat));
+        Serial.print("j");
+        Serial.println(dat[0]);
+        Serial.print("k");
+        Serial.println(dat[1]);
+        Serial.print("l");
+        Serial.println(dat[2]);
+        Serial.println();
+        break;
+     case '5': network.read(header, &dat, sizeof(dat));
+        Serial.print("m");
+        Serial.println(dat[0]);
+        Serial.print("n");
+        Serial.println(dat[1]);
+        Serial.print("o");
+        Serial.println(dat[2]);
+        Serial.println();
+        break;
+      default: network.read(header, 0, 0);
+        Serial.println(header.type);
+        break;
     }
   }
-  
-  if(!Serial) {  //check if Serial is available... if not,
-       Serial.end();      // close serial port
-       delay(100);        //wait 100 millis
-       Serial.begin(115200); // reenable serial again
-   }
- 
-  if(millis() - displayTimer > 5000){
-    displayTimer = millis();
-    Serial.println(" ");
-    Serial.println(F("********Assigned Addresses********"));
-     for(int i=0; i<mesh.addrListTop; i++){
-       Serial.print("NodeID: ");
-       Serial.print(mesh.addrList[i].nodeID);
-       Serial.print(" RF24Network Address: 0");
-       Serial.println(mesh.addrList[i].address,OCT);
-     }
-    Serial.println(F("**********************************"));
-  }
+
+  //  if(millis() - displayTimer > 5000){
+  //    displayTimer = millis();
+  //    Serial.println(" ");
+  //    Serial.println(F("********Assigned Addresses********"));
+  //     for(int i=0; i<mesh.addrListTop; i++){
+  //       Serial.print("NodeID: ");
+  //       Serial.print(mesh.addrList[i].nodeID);
+  //       Serial.print(" RF24Network Address: 0");
+  //       Serial.println(mesh.addrList[i].address,OCT);
+  //     }
+  //    Serial.println(F("**********************************"));
+  //  }
 }
